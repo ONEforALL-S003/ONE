e597c0b3dec203737251e12e87af0f449ca1a622
FantBlog <tjddnr011012@gmail.com>
2023-09-20 13:00:32 +0900

    feat : conv2d class 분리

--- compiler/q-implant/tests/q_implant_test.py
+++ compiler/q-implant/tests/q_implant_test.py
@@ -2,49 +2,9 @@
 import argparse
 import subprocess
 import os
+import importlib
 
-from test_utils import TestCase
 from test_utils import TestRunner
-from test_utils import gen_random_tensor
-
-class Conv2D_000_Q8(TestCase):
-    def __init__(self):
-        self.name = "Conv2D_000_Q8"
-
-    def generate(self) -> dict:
-        json_content = dict()
-
-        # Generate ifm
-        json_content['ifm'] = gen_random_tensor(
-            "uint8",  # dtype_str
-            (1),  # scale_shape
-            (1),  # zerop_shape
-            0)  # quantized_dimension
-
-        # Generate ker
-        json_content['ker'] = gen_random_tensor(
-            "uint8",  #dtype_str
-            (1),  # scale_shape
-            (1),  # zerop_shape
-            0,  # quantized_dimension
-            (1, 1, 1, 2))  # value_shape (OHWI)
-
-        # Generate bias
-        json_content['bias'] = gen_random_tensor(
-            "int32",  #dtype_str
-            (1),  # scale_shape
-            (1),  # zerop_shape
-            0,  # quantized_dimension
-            (1))  # value_shape
-
-        # Generate ofm
-        json_content['ofm'] = gen_random_tensor(
-            "uint8",  # dtype_str
-            (1),  # scale_shape
-            (1),  # zerop_shape
-            0)  # quantized_dimension
-
-        return json_content
     
 parser = argparse.ArgumentParser()
 parser.add_argument('--input_dir', type=str, required=True)
@@ -58,17 +18,20 @@ output_dir = args.output_dir
 driver = args.driver
 model = args.model
 
-input_circle = input_dir + '.circle'
-output_circle = output_dir + '/Conv2D_000_Q8/output.circle'
-qparam_dir = output_dir + '/Conv2D_000_Q8/qparam.json'
+module = importlib.import_module('import.' + model)
+
+input_circle = input_dir + '_000.circle'
+output_circle = output_dir + f'/{module._name_}/output.circle'
+qparam_dir = output_dir + f'/{module._name_}/qparam.json'
 
 if not os.path.exists(input_circle):
     print('fail to load input circle')
     quit(255)
 
+# generate qparam.json and numpys
 test_runner = TestRunner(output_dir)
 
-test_runner.register(Conv2D_000_Q8())
+test_runner.register(module._model_)
 
 test_runner.run()
 
@@ -76,6 +39,7 @@ if not os.path.exists(qparam_dir):
     print('qparam generate fail')
     quit(255)
 
+# run q-implant
 subprocess.run(
     [
         driver, input_circle, qparam_dir, output_circle
