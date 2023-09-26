62ebe965889f972918bc8ae988ef81ab12256a3e
FantBlog <tjddnr011012@gmail.com>
2023-09-26 16:20:50 +0900

    feat : update add op

--- compiler/q-implant-op-level-test/import/Add_000/__init__.py
+++ compiler/q-implant-op-level-test/import/Add_000/__init__.py
@@ -10,7 +10,7 @@ class Add_000_Q8(TestCase):
         json_content = dict()
 
         # Generate ifm
-        json_content['ifm'] = gen_random_tensor(
+        json_content['ifm1'] = gen_random_tensor(
             "uint8",  # dtype_str
             (1),  # scale_shape
             (1),  # zerop_shape
