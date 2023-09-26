f4d6e8385ae0442793c2e53e125c2206280c0539
FantBlog <tjddnr011012@gmail.com>
2023-09-26 15:59:49 +0900

    feat : update Add op

---
+++ compiler/q-implant-op-level-test/import/Add_002/__init__.py
@@ -0,0 +1,39 @@
+from test_utils import TestCase
+from test_utils import gen_random_tensor
+
+
+class Add_002_Q8(TestCase):
+    def __init__(self):
+        self.name = _name_
+
+    def generate(self) -> dict:
+        json_content = dict()
+
+        # Generate ifm
+        json_content['ifm'] = gen_random_tensor(
+            "uint8",  # dtype_str
+            (1),  # scale_shape
+            (1),  # zerop_shape
+            0)  # quantized_dimension
+
+        # Generate ker
+        json_content['ifm2'] = gen_random_tensor(
+            "uint8",  #dtype_str
+            (1),  # scale_shape
+            (1),  # zerop_shape
+            0,  # quantized_dimension
+            (1, 2, 2, 3))  # value_shape
+
+        # Generate ofm
+        json_content['ofm'] = gen_random_tensor(
+            "uint8",  # dtype_str
+            (1),  # scale_shape
+            (1),  # zerop_shape
+            0)  # quantized_dimension
+
+        return json_content
+
+
+_name_ = 'Add_002_Q8'
+
+_model_ = Add_002_Q8()
