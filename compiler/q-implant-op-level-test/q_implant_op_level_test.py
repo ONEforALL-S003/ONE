9bc954d355d0a52dd49575c23993ddcf9312166f
Jay <wnwoghd22@gmail.com>
2023-09-27 13:38:42 +0900

    value-test

--- compiler/q-implant-op-level-test/q_implant_op_level_test.py
+++ compiler/q-implant-op-level-test/q_implant_op_level_test.py
@@ -5,17 +5,20 @@ import os
 import importlib
 
 from test_utils import TestRunner
+from q_implant_validator import validate
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input_dir', type=str, required=True)
 parser.add_argument('--output_dir', type=str, required=True)
 parser.add_argument('--driver', type=str, required=True)
+parser.add_argument('--dump', type=str, required=True)
 parser.add_argument('--model', type=str, required=True)
 args = parser.parse_args()
 
 input_dir = args.input_dir
 output_dir = args.output_dir
 driver = args.driver
+dump = args.dump
 model = args.model
 
 module = importlib.import_module('import.' + model)
@@ -23,6 +26,7 @@ module = importlib.import_module('import.' + model)
 input_circle = input_dir + '.circle'
 output_circle = output_dir + f'/{module._name_}/output.circle'
 qparam_dir = output_dir + f'/{module._name_}/qparam.json'
+h5_dir = output_dir + f'/{module._name}/output.h5'
 
 if not os.path.exists(input_circle):
     print('fail to load input circle')
@@ -44,6 +48,13 @@ subprocess.run([driver, input_circle, qparam_dir, output_circle], check=True)
 
 if not os.path.exists(output_circle):
     print('output circle generate fail')
+    quit(255)
+
+# ./circle-tensordump --tensors_to_hdf5 ../luci/tests/Conv2D_000.circle output_path.h5
+subprocess.run([dump, '--tensors_to_hdf5', output_circle, h5_dir], check=True)
+
+if not os.path.exists(h5_dir):
+    print('h5 dump failed')
     quit(255)
 
 quit(0)
---
+++ compiler/q-implant-op-level-test/q_implant_validator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import h5py as h5
+import numpy as np
+import argparse
+import os.path
+import json
+import sys
+
+
+def validate(h5):
+    pass
